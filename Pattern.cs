a34fd07b1e8f0c667cc1211517dfa649f5433590
Meos <meos.pro@gmail.com>
2025-04-18 20:40:21 +0200

    Add Pattern class

---
+++ Pattern.cs
@@ -0,0 +1,40 @@
+﻿namespace Tim_Fill_Empty_Palette;
+
+public static class Pattern
+{
+	public static bool Match(byte[] buffer, int beginBuffer, byte[] pattern, params int[] skipIndex)
+	{
+		var viewer = new ReadOnlySpan<byte>(buffer, beginBuffer, pattern.Length);
+
+		for (int i = 0; i < pattern.Length; ++i)
+		{
+			if (skipIndex.Contains(i))
+			{
+				continue;
+			}
+
+			if (viewer[i] != pattern[i])
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static List<int> OccurenceOffsets(byte[] buffer, byte[] pattern, params int[] skipIndex)
+	{
+		var offsets = new List<int>();
+
+		for (int i = 0; i < buffer.Length - pattern.Length; ++i)
+		{
+			if (Match(buffer, i, pattern, skipIndex))
+			{
+				offsets.Add(i);
+				i += pattern.Length - 1;
+			}
+		}
+
+		return offsets;
+	}
+}
